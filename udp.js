c2764dba61ad14ec43baa5a0dcff308fa878c843
unknown <sunnepazzy123@gmail.com>
2022-03-18 21:04:56 +0100

    remove unwanted files

--- udp.js
+++
@@ -1,8 +0,0 @@
-const dgram = require('dgram');
-const socket = dgram.createSocket('udp4')
-
-socket.on('message', (msg, rinfo)=>{
-    console.log(`server get: ${msg} from ${rinfo.address}:${rinfo.port}`)
-});
-
-socket.bind(99)
