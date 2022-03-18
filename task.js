c34d43dd79b67d1fa6c862209aee150cfc959cd6
unknown <sunnepazzy123@gmail.com>
2022-03-18 20:49:18 +0100

    Remedy Product Interview Task

---
+++ task.js
@@ -0,0 +1,36 @@
+function foo(a, b) {
+    const _aDictionary = {};
+    const _bDictionary = {};
+
+    if (a.length === 0) return false;
+
+    for (let i = 0; i < a.length; i++) {
+        const key = a[i].toString();
+        if (_aDictionary[key])
+            _aDictionary[key] += 1;
+        else
+            _aDictionary[key] = 1;
+    }
+
+    for (let i = 0; i < b.length; i++) {
+        const key = b[i].toString();
+        if (_bDictionary[key])
+            _bDictionary[key] += 1;
+        else
+            _bDictionary[key] = 1;
+    }
+
+    for (let key in _aDictionary) {
+        if (!_bDictionary[key])
+            return false;
+        if (_bDictionary[key] != _aDictionary[key])
+            return false;
+    }
+
+    return true;
+}
+
+console.log(foo('abc', 'abcd'));
+console.log(foo('abc', 'abde'));
+console.log(foo('abca', 'abcd'));
+console.log(foo('bca', 'adbc'));
